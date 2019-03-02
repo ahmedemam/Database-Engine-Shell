03b7ce849d7cb0e88ec35ee22dacce730eb8dd27
ahmedemam <zizoahmed980@gmail.com>
2019-03-02 19:49:56 +0200

    initial_application_install() - error

---
+++ database_engine_shell.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+$ROOT_HOME_DIR="$HOME/Database_Engine_Shell_Source/"
+$DATABSES_DIR="$ROOT_HOME_DIR/Databases"
+$USERS_FILES_PRIVILLAGES="$ROOT_HOME_DIR/users_data"
+
+# create files and folders for 1st time application
+initial_application_install()
+{
+    # mkdir -p $DATABASES_DIR :set directory application & databsaes in once
+    # instead of 2 commands
+    mkdir $ROOT_HOME_DIR
+    mkdir $DATABSES_DIR
+    touch $USERS_FILES_PRIVILLAGES
+    cd "$ROOT_HOME_DIR"
+    echo '> SUCCESS: initial_application_install()'
+}
+
+initial_application_install
