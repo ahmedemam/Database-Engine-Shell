3bfab2701ff49ac833cbfae178ff1f36438b5cf5
MotazAbuElnasr <motazibrahim@outlook.com>
2019-03-03 04:33:33 +0200

    changing prompt colors and start reading commands needs to check for the last character (;)

--- database_engine_shell.sh
+++ database_engine_shell.sh
@@ -1,19 +1,43 @@
-#!bin/bash
+#!/usr/bin/bash
+. ./change_prompt.sh
+. ./helpers.sh
 
-$ROOT_HOME_DIR="$HOME/Database_Engine_Shell_Source/"
-$DATABSES_DIR="$ROOT_HOME_DIR/Databases"
-$USERS_FILES_PRIVILLAGES="$ROOT_HOME_DIR/users_data"
+ROOT_HOME_DIR="$HOME/Database_Engine_Shell_Source"
+DATABSES_DIR="$ROOT_HOME_DIR/Databases"
+USERS_FILES_PRIVILLAGES="$ROOT_HOME_DIR/users_data"
 
 # create files and folders for 1st time application
 initial_application_install()
 {
     # mkdir -p $DATABASES_DIR :set directory application & databsaes in once
     # instead of 2 commands
-    mkdir $ROOT_HOME_DIR
-    mkdir $DATABSES_DIR
-    touch $USERS_FILES_PRIVILLAGES
-    cd "$ROOT_HOME_DIR"
-    echo '> SUCCESS: initial_application_install()'
+    start_bash_prompt
+    if [ ! -d "$ROOT_HOME_DIR" ]; then
+        mkdir $DATABSES_DIR -p
+        touch $USERS_FILES_PRIVILLAGES
+        cd "$ROOT_HOME_DIR"
+        echo '> installation succedded :) '
+    else
+        cd "$ROOT_HOME_DIR"
+    fi
+    
 }
 
-initial_application_install
+#function for synatax error
+#if conditions for each word in the file
+
+start_app() {
+    initial_application_install
+    command="'$*'"
+    
+    while [ ${command: -1} != ";" ]
+    do
+        read -p $'\e[0;1;36m>> ' command
+        echo $command >> ./currentCommand
+        #function to read the commands and start using cases
+    done
+    read_commands
+    
+}
+
+start_app
