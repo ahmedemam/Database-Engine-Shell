de87844a394698c3b38f1de277039a88a4c6842c
ahmedemam <zizoahmed980@gmail.com>
2019-03-10 19:30:22 +0200

    select all from table

--- table_helpers/select_table.sh
+++ table_helpers/select_table.sh
@@ -1,20 +1,41 @@
 #!/usr/bin/env bash
 
 
-# SELECT * FROM TABLE
-# SELECT * FROM TABLE WHERE ....
-# SELECT * FROM TABLE WHERE .... ORDER BY ....
+# select all from tableName: 4 parameters
+# select row from tableName primary value: 6 paramters
 
-# Parameters : all - TableName - Where - Columns/Values
-# Parameters : Order By .....
-
-# databaseName - tableName - all
-function select_all_table() {
-    if [[ $3 == '*' ]] && [[ $2 != '' ]] && [[ $1 != '' ]]; then
-    # get meta_data - columns names
-    # get columns of specfiic table
-
+function select_all_from_table() {
+    if [[ $1 ]]; then
+        TABLE_META="$1_meta"
+        TABLE_NAME="$1"
+        # Metadata Printing
+        META_DATA=$(<TABLE_META)
+        set -- "$META_DATA"
+        IFS="|"; declare -a Array=($*)
+        printf "#> TABLE: ${1}:\n"
+        for (( i=0; i<${#Array[@]}-2; i++ ))
+        do
+            if [[ "${Array[i]}" != "int" ]] || [[ "${Array[i]}" != "string" ]] || [[ "${Array[i]}" != "PRIMARY" ]];
+            then
+                    printf "\t ${Array[i]}"
+            fi
+            printf "\n"
+        done
+        # Table Content Printing
+        while read -r line; do
+           set -- "${line}"
+           IFS="|"; declare -a Array=($*)
+           for (( i=0; i<${#Array[@]}; i++ ))
+           do
+                printf "\t ${Array[i]}"
+           done
+           printf "\n"
+        done < "${TABLE_NAME}"
     else
-        printf '#> \e[41mWRONG PARAMETERS.\e[49m\n'
+        printf "#>\e[41m SYNTAX ERROR.\e[41m\n"
     fi
+    read_commands
 }
+
+# TABLE META
+# COL.NAME|DATATYPE|COL.NAME|DATATYPE|PRIMARY|COL.NAME
